c187d663abca9d30d06dbd5a92971411325297ca
Priyanka Yadav <priyanka.yadav@promobitech.com>
2021-07-19 18:53:53 +0530

    + Adding Hello module + Calling gretting module Hello Func from Hello module

---
+++ hello/go.mod
@@ -0,0 +1,5 @@
+module example.com/hello
+
+go 1.14
+
+require github.com/Priyanka4328/go-modules/grettings v0.0.0-20210719131529-77e336e6be37
---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Priyanka4328/go-modules/grettings"
+)
+
+
+func main(){
+	message := grettings.Hello("Priyanka")
+	fmt.Println(message)
+}
